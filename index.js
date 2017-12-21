e0614063e5ae0553b40a845c95ff10348b5cca97
nuintun <nuintun@qq.com>
2017-12-21 15:57:38 +0800

    :art: Update browsers

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ module.exports = function(options) {
     {
       add: true,
       remove: true,
-      browsers: ['>= 0.5% in CN', '>= 1%', 'ie >= 8']
+      browsers: ['> 0.5% in CN', '> 1%', 'ie >= 8']
     },
     options.autoprefixer
   );
