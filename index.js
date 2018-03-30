23131087e89199c90c6855a520c44afdf93b123a
nuintun <nuintun@qq.com>
2018-03-30 22:04:17 +0800

    :art: Rewrite

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ module.exports = function(options = {}) {
 
   return {
     name: 'gulp-css-plugins',
-    async bundle(path, contents) {
+    async transformed(path, contents) {
       if (!isFileType(path, 'css')) return contents;
 
       // Get contents string
